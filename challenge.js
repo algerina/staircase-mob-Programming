37b7535c6b6b993ed771f0a28efaac12fbce8b2f
Amel Khiri <kh.amelle@gmail.com>
2022-02-25 15:27:36 +0100

    Create challenge.js

---
+++ challenge.js
@@ -0,0 +1,12 @@
+
+function staircase(n) {
+    // Write your code here
+    let lines = ""
+
+    for (let i = 1; i <= n; i++) {
+    lines += "#" 
+    console.log(" ".repeat(n-i) + lines)
+
+    }
+    
+}
